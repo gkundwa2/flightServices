471a66aaf3915b6cf35f40ebfec420eb2194e5b9
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2020-12-11 10:50:20 -0600

    this is my project

---
+++ flightApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
+from flightApp.models import Flight,Passenger,Reservation
+from flightApp.serializers import FlightSerializer, PassengerSerializer, ReservationSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['POST'])
+def find_flights(request):
+    flights=Flight.objects.filter(departureCity=request.data['departureCity'],arrivalCity=request.data['arrivalCity'],dateOfDeparture=request.data['dateOfDeparture'])
+    serializer=FlightSerializer(flights,many=True)
+    return Response(serializer.data)
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset=Flight.objects.all()
+    serializer_class=FlightSerializer
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset=Passenger.objects.all()
+    serializer_class=PassengerSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset=Reservation.objects.all()
+    serializer_class=ReservationSerializer
